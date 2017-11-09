8601f5d2bfb36c3962cf5a2b11cf53d80edb1d3e
Maciej Znamirowski <maciek@znamirowski.pl>
2017-11-09 22:34:06 +0100

    working navbar and top page container with the slogan

--- src/app.js
+++ src/app.js
@@ -1,5 +1,39 @@
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+  configureRouter(config, router) {
+    config.title = 'CEC';
+    config.map([
+      {
+        route: ['', 'home'], name: 'home',
+        nav: true,
+        moduleId: './pages/home/home', title: 'Home'
+      },
+      {
+        route: 'about', name: 'about',
+        nav: true,
+        moduleId: './pages/about/about', title: 'About'
+      },
+      {
+        route: 'program', name: 'program',
+        nav: true,
+        moduleId: './pages/program/program', title: 'Program'
+      },
+      {
+        route: 'workshops', name: 'workshops',
+        nav: true,
+        moduleId: './pages/workshops/workshops', title: 'Workshops'
+      },
+      {
+        route: 'registration', name: 'registration',
+        nav: true,
+        moduleId: './pages/registration/registration', title: 'Registration'
+      },
+      {
+        route: 'contact', name: 'contact',
+        nav: true,
+        moduleId: './pages/contact/contact', title: 'Contact'
+      }
+    ]);
+
+    this.router = router;
   }
 }
---
+++ src/pages/workshops/workshops.js
@@ -0,0 +1,3 @@
+export class Workshops {
+  message = 'My Workshops';
+}
