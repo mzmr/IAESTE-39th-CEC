23f8a21b3d895c279db1c21c3d4b114b6e4f95d4
Maciej Znamirowski <maciek@znamirowski.pl>
2017-12-08 01:09:53 +0100

    Upgraded navbar

--- src/resources/elements/menu.js
+++ src/resources/elements/menu.js
@@ -2,4 +2,24 @@ import {bindable} from 'aurelia-framework';
 
 export class Menu {
   @bindable router = null;
+
+  attached() {
+    $('[data-spy="affix"]').each(function() {
+      let $spy = $(this);
+      let data = $spy.data();
+
+      data.offset = data.offset || {};
+
+      if (data.offsetBottom !== null) data.offset.bottom = data.offsetBottom;
+      if (data.offsetTop    !== null) data.offset.top    = data.offsetTop;
+
+      $spy.affix(data);
+    });
+
+    $(document).ready(function() {
+      $('.navbar-nav li.trigger-collapse a').click(function(event) {
+        $('.navbar-collapse').collapse('hide');
+      });
+    });
+  }
 }
