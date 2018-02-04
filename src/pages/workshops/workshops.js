ca4860033fef5c9bdf203c8f6e4fdaf7100a5dba
Maciej Znamirowski <maciek@znamirowski.pl>
2018-02-04 17:29:37 +0100

    Some more info about bus arrival. Minor fixes.

--- src/pages/workshops/workshops.js
+++ src/pages/workshops/workshops.js
@@ -14,18 +14,6 @@ export class Workshops {
 
   attached() {
     addBackgrounds();
-
-    gmap.load().then(() => {
-      let uluru = {lat: -25.363, lng: 131.044};
-      let map = new google.maps.Map(document.getElementById('mapp'), {
-        zoom: 4,
-        center: uluru
-      });
-      let marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-      });
-    });
   }
 
   created() {
